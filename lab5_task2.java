5b3e5c1cbf0996ed59a4d20b76a7cb7e43936802
BabaninGA <far_nj@mail.ru>
2021-12-21 20:48:50 +0300

    Add file

--- lab5_task2.java
+++ lab5_task2.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class Taks2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N=0;
+        int N = 0;
         try {
             System.out.println("Введите количество элементов в списке");
             N = in.nextInt();
@@ -17,20 +17,20 @@ public class Taks2 {
             System.out.println("Количество элементов списка может быть задано только целым числом");
             System.exit(1);
         }
-        if (N>=1) {
+        if (N >= 1) {
             ArrayList<Integer> Alist = new ArrayList<>();
             try {
                 System.out.println("Введите элементы списка");
                 for (int i = 0; i < N; i++) {
-                Alist.add(i, in.nextInt());
-            }
+                    Alist.add(i, in.nextInt());
+                }
             } catch (InputMismatchException e) {
                 System.out.println("Элементами списка в данной программе могут быть только целые числа");
                 System.exit(1);
             }
             LinkedList<Integer> AlistNew = new LinkedList<>();
             AlistNew.addAll(Alist);
-            int X=0;
+            int X = 0;
             try {
                 System.out.println("Введите значение. Все элементы заданного Вами списка, равные этому значению, будут удалены");
                 X = in.nextInt();
